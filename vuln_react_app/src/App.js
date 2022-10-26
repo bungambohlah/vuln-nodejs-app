aea7e3482ffec544ac44324553a858fe6fb23548
Afif Abdillah Jusuf <bungambohlah@gmail.com>
2022-10-26 20:59:11 +0700

    feat: :sparkles: add reacthref xss implementation

--- vuln_react_app/src/App.js
+++ vuln_react_app/src/App.js
@@ -1,19 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './MyComponents/Header';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import React_href_xss from './MyComponents/React_href_xss';
-import React_ref_innerHTML_xss from './MyComponents/React_ref_innerHTML_xss';
+import "./App.css";
+import Header from "./MyComponents/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ReactHrefXss from "./MyComponents/ReactHrefXss";
+// import React_ref_innerHTML_xss from "./MyComponents/React_ref_innerHTML_xss";
 function App() {
   return (
     <>
-    <Header/>
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/react-href-xss" element={<React_href_xss/>}/>
-        <Route exact path="/react-ref-innerHTML-xss"  element={<React_ref_innerHTML_xss/>}/>     
-      </Routes>
-    </BrowserRouter>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/react-href-xss" element={<ReactHrefXss />} />
+          {/* <Route
+            exact
+            path="/react-ref-innerHTML-xss"
+            element={<React_ref_innerHTML_xss />}
+          /> */}
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ vuln_react_app/src/MyComponents/ReactHrefXss.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+
+export default function React_href_xss() {
+  const [state, setState] = useState({ name: "", email: "", website: "" });
+
+  function updateForm() {
+    document.getElementById("updated").setAttribute("hidden", true);
+    document.getElementById("update").removeAttribute("hidden");
+  }
+
+  async function saveData() {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const website = document.getElementById("website").value;
+    const request = await fetch(`${window.location.origin}/react-xss`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({ name: name, email: email, website: website }),
+    });
+    const response = await request.json();
+    // console.log(response);
+    setState({
+      name: response.name,
+      email: response.email,
+      website: response.website,
+    });
+    document.getElementById("update").setAttribute("hidden", true);
+    document.getElementById("updated").removeAttribute("hidden");
+  }
+
+  return (
+    <div>
+      <br />
+      <div className="container">
+        <h2>React XSS</h2>
+        <br />
+        <p>
+          Application is using ReactJS which provides by default protection from
+          XSS attacks by encoding dangerous characters before appending it to
+          the page but there are cases when it will not protect you from XSS
+          attacks. In this exercise you will learn how you can perform XSS when
+          application passes user supplied input to <b>href</b> attribute.
+        </p>
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">Update Profile</h5>
+            <div id="update">
+              <p className="card-text">
+                Name: <input type="text" id="name"></input>
+                <br />
+                <br />
+                Email: <input type="email" id="email"></input>
+                <br />
+                <br />
+                Website:{" "}
+                <input
+                  type="website"
+                  id="website"
+                  placeholder="https://example.com"
+                ></input>{" "}
+                <br />
+                <br />
+                <button onClick={saveData}>Save</button>
+              </p>
+            </div>
+            <div id="updated" hidden>
+              <p className="card-text">
+                Name: {state.name} <br />
+                Email: {state.email} <br />
+                Website: <a href={state.website}>{state.website}</a> <br />
+                <br />
+                <button onClick={updateForm}>Edit</button>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
